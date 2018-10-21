177ed5f6d2d15c447bf6fd7d01383e3d5b8481a9
SlowV <31509114+SlowV@users.noreply.github.com>
2018-10-21 23:46:26 +0700

    fix seed

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -16,26 +16,31 @@ class CategorySeeder extends Seeder
                 'id'=>1,
                 'name'=>'Hoa chúc mừng',
                 'description'=>'Hoa chúc mừng',
+                'images' => 'collection1.jpg'
             ],
             [
                 'id'=>2,
                 'name'=>'Hoa sinh nhật',
                 'description'=>'Hoa sinh nhật',
+                'images' => 'collection1.jpg'
             ],
             [
                 'id'=>3,
                 'name'=>'Hoa tình yêu',
                 'description'=>'Hoa tình yêu',
+                'images' => 'collection1.jpg'
             ],
             [
                 'id'=>4,
                 'name'=>'Hoa giỏ, hộp',
                 'description'=>'Hoa giỏ, hộp',
+                'images' => 'collection1.jpg'
             ],
             [
                 'id'=>5,
                 'name'=>'Hoa cưới',
                 'description'=>'Hoa cưới',
+                'images' => 'collection1.jpg'
             ],
         ]);
     }
